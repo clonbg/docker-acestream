b6608da2b20113f57c7aa38466c45f5ea11aba86
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2019-10-10 21:35:17 +0800

    update run.sh
    add port mapping and set bigger video buffer disk space

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
-DOCKER_IMAGE_NAME="jackwzh/acestream-server-new"
+DOCKER_IMAGE_NAME="jackwzh/acestream-server"
 SERVER_HTTP_PORT="6878"
 
 docker run \
 	--name ace-server \
 	--publish "$SERVER_HTTP_PORT:$SERVER_HTTP_PORT" \
+	-p 8621:8621 \
 	--rm \
 	--mount type=tmpfs,target=/dev/disk/by-id,tmpfs-mode=660,tmpfs-size=4k \
-	--mount type=tmpfs,target=/root/.ACEStream,tmpfs-mode=660,tmpfs-size=4096m \
+	--mount type=tmpfs,target=/root/.ACEStream,tmpfs-mode=660,tmpfs-size=8192m \
 	"$DOCKER_IMAGE_NAME"
